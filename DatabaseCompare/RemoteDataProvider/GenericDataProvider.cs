8c1a5b52460fef73f4da8668994bbfd950dabc7c
Bernhard Wendel <bwendel@gmx.de>
2017-07-23 19:36:15 +0200

    Adding MYSQL Data Provider

---
+++ DatabaseCompare/RemoteDataProvider/GenericDataProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace DatabaseCompare.RemoteDataProvider
+{
+    abstract class GenericDataProvider
+    {
+        protected string ConnectionString;
+        protected Regex MatchDatabasesRegex;
+        protected Regex MatchSchemaRegex;
+        protected DataContext db;
+
+        public Action<int> OnMaxKnown { get; set; }
+        public Action<int, string> OnProgress { get; set; }
+
+        public GenericDataProvider(DataContext db)
+        {
+            this.ConnectionString = ConfigurationManager.AppSettings["RemoteDataConnectionString"];
+            this.db = db;
+            this.MatchDatabasesRegex = new Regex(ConfigurationManager.AppSettings["RemoteDataDatabaseRegex"]);
+            this.MatchSchemaRegex = new Regex(ConfigurationManager.AppSettings["RemoteDataSchemaRegex"]);
+        }
+
+        protected abstract void Connect();
+
+        protected abstract List<string> ListDatabases();
+
+        protected abstract void LoadColumnsFromDatabase(string database);
+
+        public void RefreshColumns()
+        {
+            using (var Transaction = db.Database.BeginTransaction()) {
+                try
+                {
+                    db.Database.ExecuteSqlCommand("DELETE FROM DBSchema;");
+
+                    // Connect -> List Databases -> Filter Databases -> Load Columns for each filtered Database
+                    this.Connect();
+
+                    List<string> Databases = this.ListDatabases();
+                    Databases = this.FilterDatabases(Databases);
+
+                    // Tell the caller, we can now predct a progress
+                    OnMaxKnown?.Invoke(Databases.Count);
+
+                    for (int i = 0; i < Databases.Count; i++)
+                    {
+                        OnProgress?.Invoke(i + 1, Databases[i]);
+                        LoadColumnsFromDatabase(Databases[i]);
+                    }
+
+                    db.SaveChanges();
+                    Transaction.Commit();
+
+                }
+                catch (Exception)
+                {
+                    Transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        private List<string> FilterDatabases(List<string> databases)
+        {
+            List<string> Result = databases.Where(item => (this.MatchDatabasesRegex.Match(item).Success)).ToList();
+            return Result;
+        }
+    }
+}
---
+++ DatabaseCompare/RemoteDataProvider/MySQLDataProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DatabaseCompare.RemoteDataProvider
+{
+    class MySQLDataProvider : GenericDataProvider
+    {
+        private MySqlConnection client;
+
+        public MySQLDataProvider(DataContext db) : base(db)
+        {
+            client = new MySqlConnection();
+        }
+
+        override protected void Connect()
+        {
+            client.ConnectionString = this.ConnectionString;
+            client.Open();
+        }
+
+        override protected List<string> ListDatabases()
+        {
+            List<string> result = new List<string>();
+
+            using (MySqlCommand command = client.CreateCommand())
+            {
+                command.CommandText = "SHOW DATABASES";
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(reader.GetValue(0).ToString());
+                    }
+                }
+
+            }
+            return result;
+        }
+
+        override protected void LoadColumnsFromDatabase(string database)
+        {
+            client.ChangeDatabase("INFORMATION_SCHEMA");
+
+            using (MySqlCommand command = client.CreateCommand())
+            {
+                command.CommandText = "SELECT TABLE_NAME, COLUMN_NAME, COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = @databaseName";
+                command.Parameters.AddWithValue("@databaseName", database);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        db.Fields.Add(new DBSchema { DatabaseName = database, Schema = "", TableName = reader.GetString(0), FieldName = reader.GetString(1), DataType = reader.GetString(2) });
+                    }
+                }
+            }
+
+        }
+    }
+}
