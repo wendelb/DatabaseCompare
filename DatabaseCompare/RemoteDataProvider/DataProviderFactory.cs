9b3ef7dddbfb8ebdc9534310bfa0b20cdbc0f51f
Bernhard Wendel <bwendel@gmx.de>
2017-07-23 20:37:38 +0200

    Adding DataProviderFactory

---
+++ DatabaseCompare/RemoteDataProvider/DataProviderFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+
+namespace DatabaseCompare.RemoteDataProvider
+{
+    class DataProviderFactory
+    {
+        public static GenericDataProvider createDataProvider(DataContext db)
+        {
+            string DataProvider = ConfigurationManager.AppSettings["RemoteDataProvider"];
+
+            if (DataProvider == "postgres")
+            {
+                return new PostgreSQLDataProvider(db);
+            }
+            else if (DataProvider == "mysql")
+            {
+                return new MySQLDataProvider(db);
+            }
+            else
+            {
+                throw new Exception(String.Format("DataProvider {0} is unknown", DataProvider));
+            }
+        }
+    }
+}
