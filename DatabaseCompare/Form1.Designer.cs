c67ebc09123467b4645af8877f4d32e2da920683
Bernhard Wendel <bwendel@gmx.de>
2017-07-23 22:44:04 +0200

    The main application

--- DatabaseCompare/Form1.Designer.cs
+++ DatabaseCompare/Form1.Designer.cs
@@ -28,12 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.remoteDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshFieldsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterResultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showDifferencesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showAllFieldsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.FieldsView = new System.Windows.Forms.ListView();
+            this.columnHeaderDatabase = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSchema = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderTable = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderField = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderDataType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.remoteDatabaseToolStripMenuItem,
+            this.filterToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(910, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "&Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // remoteDatabaseToolStripMenuItem
+            // 
+            this.remoteDatabaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshFieldsToolStripMenuItem});
+            this.remoteDatabaseToolStripMenuItem.Name = "remoteDatabaseToolStripMenuItem";
+            this.remoteDatabaseToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.remoteDatabaseToolStripMenuItem.Text = "Remote &Database";
+            // 
+            // refreshFieldsToolStripMenuItem
+            // 
+            this.refreshFieldsToolStripMenuItem.Name = "refreshFieldsToolStripMenuItem";
+            this.refreshFieldsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.refreshFieldsToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.refreshFieldsToolStripMenuItem.Text = "&Refresh Fields";
+            this.refreshFieldsToolStripMenuItem.Click += new System.EventHandler(this.refreshFieldsToolStripMenuItem_Click);
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filterResultsToolStripMenuItem,
+            this.showDifferencesToolStripMenuItem,
+            this.showAllFieldsToolStripMenuItem});
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.filterToolStripMenuItem.Text = "F&ilter";
+            // 
+            // filterResultsToolStripMenuItem
+            // 
+            this.filterResultsToolStripMenuItem.Name = "filterResultsToolStripMenuItem";
+            this.filterResultsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.filterResultsToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.filterResultsToolStripMenuItem.Text = "Filter &Results";
+            this.filterResultsToolStripMenuItem.Click += new System.EventHandler(this.filterResultsToolStripMenuItem_Click);
+            // 
+            // showDifferencesToolStripMenuItem
+            // 
+            this.showDifferencesToolStripMenuItem.Enabled = false;
+            this.showDifferencesToolStripMenuItem.Name = "showDifferencesToolStripMenuItem";
+            this.showDifferencesToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.showDifferencesToolStripMenuItem.Text = "Show Differences";
+            // 
+            // showAllFieldsToolStripMenuItem
+            // 
+            this.showAllFieldsToolStripMenuItem.Checked = true;
+            this.showAllFieldsToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.showAllFieldsToolStripMenuItem.Name = "showAllFieldsToolStripMenuItem";
+            this.showAllFieldsToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.showAllFieldsToolStripMenuItem.Text = "&Show all Fields";
+            this.showAllFieldsToolStripMenuItem.Click += new System.EventHandler(this.showAllFieldsToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 432);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(910, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel.Text = "Ready";
+            // 
+            // FieldsView
+            // 
+            this.FieldsView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderDatabase,
+            this.columnHeaderSchema,
+            this.columnHeaderTable,
+            this.columnHeaderField,
+            this.columnHeaderDataType});
+            this.FieldsView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FieldsView.FullRowSelect = true;
+            this.FieldsView.GridLines = true;
+            this.FieldsView.Location = new System.Drawing.Point(0, 24);
+            this.FieldsView.MultiSelect = false;
+            this.FieldsView.Name = "FieldsView";
+            this.FieldsView.Size = new System.Drawing.Size(910, 408);
+            this.FieldsView.TabIndex = 2;
+            this.FieldsView.UseCompatibleStateImageBehavior = false;
+            this.FieldsView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderDatabase
+            // 
+            this.columnHeaderDatabase.Text = "Database";
+            // 
+            // columnHeaderSchema
+            // 
+            this.columnHeaderSchema.Text = "Schema";
+            // 
+            // columnHeaderTable
+            // 
+            this.columnHeaderTable.Text = "Table";
+            // 
+            // columnHeaderField
+            // 
+            this.columnHeaderField.Text = "Field";
+            // 
+            // columnHeaderDataType
+            // 
+            this.columnHeaderDataType.Text = "DataType";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(910, 454);
+            this.Controls.Add(this.FieldsView);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Database Compare";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem remoteDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshFieldsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filterResultsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showDifferencesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showAllFieldsToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ListView FieldsView;
+        private System.Windows.Forms.ColumnHeader columnHeaderDatabase;
+        private System.Windows.Forms.ColumnHeader columnHeaderSchema;
+        private System.Windows.Forms.ColumnHeader columnHeaderTable;
+        private System.Windows.Forms.ColumnHeader columnHeaderField;
+        private System.Windows.Forms.ColumnHeader columnHeaderDataType;
     }
 }
 
--- DatabaseCompare/Form1.cs
+++ DatabaseCompare/Form1.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.Entity;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using DatabaseCompare.RemoteDataProvider;
+using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,14 +7,142 @@ namespace DatabaseCompare
 {
     public partial class FormMain : Form
     {
+        /// <summary>
+        /// Instance of the filter class
+        /// </summary>
+        private DisplayFilter filter;
+
         public FormMain()
         {
             // Initialize the form
             InitializeComponent();
 
             // Initialize the database
-            new MigrationManager();
+            try
+            {
+                new MigrationManager();
+                this.db = new DataContext();
+                this.filter = new DisplayFilter(this.db.Fields);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("An error occured during initialization:" + Environment.NewLine + ex.Message + Environment.NewLine + Environment.NewLine + ex.StackTrace, "Database Compare: Failed to load from remote database", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+        }
 
+        /// <summary>
+        /// Private reference to the Database Connection
+        /// </summary>
+        private DataContext db;
+
+        /// <summary>
+        /// Action Handler for the Click Event on the <i>Fiel</i> -> <i>Quit</i> Menu Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Action Handler for the Click Event on the <i>Filter</i> -> <i>Filter Results</i> Menu Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void filterResultsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (FormFilter formFilter = new FormFilter(db))
+            {
+                if (formFilter.ShowDialog() == DialogResult.OK)
+                {
+                    filter.filterSchema = formFilter.filterSchema;
+                    filter.filterTable = formFilter.filterTable;
+                    filter.filterColumn = formFilter.filterColumn;
+
+                    // Update Checkboxes
+                    showAllFieldsToolStripMenuItem.Checked = false;
+                    filterResultsToolStripMenuItem.Checked = true;
+                    showDifferencesToolStripMenuItem.Checked = false;
+
+                    // After the filter has been updated, refresh the display
+                    RefreshFieldsView();
+                }
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            // Display all Data on Load
+            RefreshFieldsView();
+        }
+
+        /// <summary>
+        /// Clear and load the filtered data into the main control.
+        /// Also takes care of column resizing
+        /// </summary>
+        private void RefreshFieldsView()
+        {
+            FieldsView.BeginUpdate();
+
+            FieldsView.Items.Clear();
+            foreach (var field in filter.applyFilter())
+            {
+                string[] data = { field.DatabaseName, field.Schema, field.TableName, field.FieldName, field.DataType };
+                FieldsView.Items.Add(new ListViewItem(data));
+            }
+
+            FieldsView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            FieldsView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            FieldsView.EndUpdate();
+        }
+
+        /// <summary>
+        /// Action Handler for the Click Event on the <i>Remote Database</i> -> <i>Refresh Fields</i> Menu Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void refreshFieldsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel.Text = "Loading Databases...";
+            try
+            {
+                var DataPrivoder = DataProviderFactory.createDataProvider(this.db);
+                //DataPrivoder.OnMaxKnown
+                DataPrivoder.OnProgress = ((i, database) => { toolStripStatusLabel.Text = "Loading Columns from " + database; });
+
+                await Task.Run(() => DataPrivoder.RefreshColumns());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("An error occured while loading data from the remote database" + Environment.NewLine + ex.Message + Environment.NewLine + Environment.NewLine + ex.StackTrace, "Database Compare: Failed to load from remote database", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                toolStripStatusLabel.Text = "Ready";
+                RefreshFieldsView();
+            }
+        }
+
+        /// <summary>
+        /// Action Handler for the Click Event on the <i>Filter</i> -> <i>Show all Fields</i> Menu Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showAllFieldsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Update Checkboxes
+            showAllFieldsToolStripMenuItem.Checked = true;
+            filterResultsToolStripMenuItem.Checked = false;
+            showDifferencesToolStripMenuItem.Checked = false;
+
+            // Clear the filter
+            filter.Clear();
+
+            // Refresh the View
+            RefreshFieldsView();
         }
     }
 }
