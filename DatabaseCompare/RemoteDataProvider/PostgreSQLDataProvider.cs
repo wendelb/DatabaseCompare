be0ca291091139c1ee11290f64a49a12d4ae89fe
Bernhard Wendel <bwendel@gmx.de>
2017-07-23 20:34:15 +0200

    Adding PostgresDataProvider

---
+++ DatabaseCompare/RemoteDataProvider/PostgreSQLDataProvider.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Npgsql;
+
+namespace DatabaseCompare.RemoteDataProvider
+{
+    class PostgreSQLDataProvider : GenericDataProvider
+    {
+
+        private NpgsqlConnection client;
+
+        public PostgreSQLDataProvider(DataContext db) : base(db)
+        {
+        }
+
+        protected override void Connect()
+        {
+            client = new NpgsqlConnection();
+            client.ConnectionString = this.ConnectionString;
+            client.Open();
+        }
+
+        protected override void Disconnect()
+        {
+            client.Close();
+            client.Dispose();
+            client = null;
+        }
+
+        protected override List<string> ListDatabases()
+        {
+            List<string> result = new List<string>();
+
+            using (NpgsqlCommand command = client.CreateCommand())
+            {
+                command.CommandText = "SELECT datname FROM pg_database WHERE datistemplate = false and datallowconn = true;";
+
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(reader.GetValue(0).ToString());
+                    }
+                }
+
+            }
+            return result;
+        }
+
+        protected override void LoadColumnsFromDatabase(string database)
+        {
+            client.ChangeDatabase(database);
+
+            using (NpgsqlCommand command = client.CreateCommand())
+            {
+                command.CommandText = @"SELECT c.table_schema, c.table_name, c.column_name, CASE WHEN character_maximum_length is null THEN c.data_type ELSE CONCAT(c.data_type, '(', c.character_maximum_length, ')') END as data_type
+FROM INFORMATION_SCHEMA.COLUMNS c
+INNER JOIN INFORMATION_SCHEMA.TABLES t USING (table_catalog, table_schema, table_name)
+WHERE c.table_schema not in ('pg_catalog', 'information_schema') and t.table_type = 'BASE TABLE'
+ORDER BY table_schema, table_name, column_name, ordinal_position";
+
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        db.Fields.Add(new DBSchema { DatabaseName = database, Schema = reader.GetString(0), TableName = reader.GetString(1), FieldName = reader.GetString(2), DataType = reader.GetString(3) });
+                    }
+                }
+            }
+        }
+    }
+}
