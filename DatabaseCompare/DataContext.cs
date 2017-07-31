f8dfe3bd29d330b652034af3d8a9076fe9f65229
Bernhard Wendel <bwendel@gmx.de>
2017-07-31 20:41:41 +0200

    DataContext: Set Journal to Memory

--- DatabaseCompare/DataContext.cs
+++ DatabaseCompare/DataContext.cs
@@ -6,6 +6,18 @@ namespace DatabaseCompare
 {
     public class DataContext : DbContext
     {
+        public DataContext(): base()
+        {
+            // Set Journal to Memory after Initialization
+            // As we can recover from any situation by just re-retrieving the Data from the Main Database, this will be fine!
+            this.Database.Connection.Open();
+            using (var command = this.Database.Connection.CreateCommand())
+            {
+                command.CommandText = "PRAGMA journal_mode = MEMORY;";
+                command.ExecuteNonQuery();
+            }
+        }
+
         public DbSet<SchemaInfo> SchemaInfo { get; set; }
         public DbSet<DBSchema> Fields { get; set; }
     }
